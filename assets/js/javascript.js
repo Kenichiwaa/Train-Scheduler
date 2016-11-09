706c3620b602be5e1405340195565d98b80fb5e2
Kenichiwaa <kenichiwa.ninja@gmail.com>
2016-11-08 21:05:38 -0800

    pushing to heroku

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,13 +1,17 @@
 
+// GLOBAL VARIABLES // Arrays and variables for holding data
+// =================================================================
+
+
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBtQ0s6D2BqAntMzT_bZlhV82wm6SxVZBA",
-    authDomain: "train-time-73250.firebaseapp.com",
-    databaseURL: "https://train-time-73250.firebaseio.com",
-    storageBucket: "train-time-73250.appspot.com",
-    messagingSenderId: "358539235836"
-  };
-  firebase.initializeApp(config);
+	var config = {
+		apiKey: "AIzaSyBtQ0s6D2BqAntMzT_bZlhV82wm6SxVZBA",
+		authDomain: "train-time-73250.firebaseapp.com",
+		databaseURL: "https://train-time-73250.firebaseio.com",
+		storageBucket: "train-time-73250.appspot.com",
+		messagingSenderId: "358539235836"
+	};
+	firebase.initializeApp(config);
 
 	var database = firebase.database();
 
@@ -17,8 +21,11 @@
 	var firstTrainTime = "";
 	var frequency = "";
 	
+// MAIN PROCESS
+// =================================================================
+
 	$('#submit').on('click', function(){
-			
+
 			// grab text from input field, grab value & trim blank spaces before and after 
 		  	trainName = $('#trainName').val().trim();
 		  	destination = $('#destination').val().trim();
@@ -26,15 +33,65 @@
 		  	frequency = $('#frequency').val().trim();
 
 		  	// pushing object data to firebase
-		  	database.ref().set({
-		  		name: firebase.database.ServerValue.TIMESTAMP,
+		  	database.ref().push({
 		  		train: trainName,
 		  		destination : destination,
 		  		trainTime: firstTrainTime,
-				frequency: frequency
+				frequency: frequency,
 		  	});
 
+	// Don't refresh the page!
 	return false; 
 	}); // End #submit click function
 
+	// Create Firebase "watcher" (.on("value"))
+	database.ref().on('child_added', function(snapshot){
 
+			var newTrainName = snapshot.val().train;
+			var newDestination = snapshot.val().destination;
+			var newFirstTrainTime = snapshot.val().trainTime;
+			var newFrequency = snapshot.val().frequency;
+
+			// Moment ============================================================
+
+			  	// First Time (pushed back 1 year to make sure it comes before current time)
+			  	var firstTimeConverted = moment(newFirstTrainTime,"hh:mm").subtract(1, "years");
+			  	console.log(firstTimeConverted);
+
+			  	// Current Time
+			  	var currentTimeMoment = moment();
+			  	console.log("Current Time: " + moment(currentTimeMoment).format("hh:mm"));
+
+			  	//Difference between the times
+			  	var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+			  	console.log("Difference In Time: " + diffTime);
+
+			  	// Time apart (remainder)
+			  	var tRemainder = diffTime % newFrequency;
+			  	console.log(tRemainder);
+
+			  	// Minute Until Train
+			  	var tMinutesUntilTrain = newFrequency - tRemainder;
+			  	console.log("Minutes Until Train: " + tMinutesUntilTrain);
+
+			  	// Next Train
+			  	var nextTrain = moment().add(tMinutesUntilTrain, "minutes");
+			  	console.log("Arrival Time: " + moment(nextTrain).format("hh:mm"));
+
+			  	// Next Train Arrival Time
+			  	var nextTrainArrivalTime = moment(nextTrain).format("hh:mm");
+
+			// End Moment ============================================================
+
+			// Log everthing that's coming out of snapshot
+			console.log(snapshot.val());
+			console.log('train: '+ snapshot.val().train);
+			console.log('destination: '+ snapshot.val().destination);
+			console.log('train time: '+ snapshot.val().trainTime);
+			console.log('frequency: '+ snapshot.val().frequency);
+			console.log('next train: '+ nextTrainArrivalTime);
+			console.log('minutes away: '+ tMinutesUntilTrain);
+
+		$(".trainInformation").append('<tr><td>'+ newTrainName +'</td><td>'+ newDestination +'</td><td>'+ newFrequency +'</td><td>'+ nextTrainArrivalTime +'</td><td>'+ tMinutesUntilTrain +'</td></tr>');
+
+	}); // End database.ref().on('value', function(snapshot){
